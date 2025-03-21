e38195b2362e8d4ab285a17be8224f9140bab8bd
kylezxc_ <114975031+kyleezxc@users.noreply.github.com>
2025-03-21 21:51:41 +0800

    Update StackNode.java

--- StackNode.java
+++ StackNode.java
@@ -1,14 +1,17 @@
+import java.time.LocalDate;
+
 public class StackNode {
     StackNode next;
-    String bookTitle, bookAuthor, bookBorrower, dateBorrowed;
+    String bookTitle, bookAuthor, bookBorrower;
     int bookID;
-
-    StackNode(int bookID, String bookTitle, String bookAuthor, String bookBorrower, String dateBorrowed){
+    LocalDate date = LocalDate.now();
+    
+    StackNode(int bookID, String bookTitle, String bookAuthor, String bookBorrower, LocalDate date){
         this.bookID = bookID;
         this.bookTitle = bookTitle;
         this.bookAuthor = bookAuthor;
         this.bookBorrower = bookBorrower;
-        this.dateBorrowed = dateBorrowed;
+        this.date = date;
         this.next = null;
     }
 }
